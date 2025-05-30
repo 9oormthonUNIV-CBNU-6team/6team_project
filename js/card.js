ec685f2a208aa9398fb8ba059b29dc9a1e7f1a0e
hsssj4 <81948488+hsssj4@users.noreply.github.com>
2025-05-30 12:08:40 +0900

    면접 카드게임 화면 구현
    summary
    면접 카드게임 화면 구현
    
    api 기능 미구현 상태
    
    png 파일 추가

--- js/card.js
+++ js/card.js
@@ -13,6 +13,7 @@ const cardData = {
 		{ text: "질문 재확인 후 답변", percent: 16 },
 	]
 };
+
 window.onload = () => {
 	document.getElementById('question').innerText = cardData.question;
 	document.getElementById('company').innerText = cardData.company;
