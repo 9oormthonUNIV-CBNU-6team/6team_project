cc166758cb2f759edaac243a641519388ef132d4
hsssj4 <hongseongjin123@gmail.com>
2025-06-01 21:40:05 +0900

    최종 배포

--- js/welcome.js
+++ js/welcome.js
@@ -2,5 +2,5 @@ const nickname = localStorage.getItem("nickname") || "사용자";
 document.getElementById("nickname").textContent = nickname;
 
 function goToLogin() {
-	window.location.href = "login.html";
+	window.location.href = "index.html";
 }
